26b4c5611074529a0d893e006ea0b115625f45e9
tradingmany127-byte <tradingmany127@gmail.com>
2026-02-25 00:01:02 +0100

    E

--- server.js
+++ server.js
@@ -214,4 +214,7 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server started on port", PORT));
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server started on port", PORT);
+});
